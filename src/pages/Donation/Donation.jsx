330860e719e267c59328db2403b320da36b56c97
MHShihan <mahmodul.no.1@gmail.com>
2023-09-30 23:18:01 +0600

    See All button has created

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -4,6 +4,7 @@ import DonatedItems from "../../components/DonatedItems/DonatedItems";
 const Donation = () => {
   const [donations, setDonations] = useState([]);
   const [notFound, setNotFound] = useState("");
+  const [isShowAll, setIsShowAll] = useState(false);
 
   useEffect(() => {
     const donatedItem = JSON.parse(localStorage.getItem("donations"));
@@ -14,8 +15,14 @@ const Donation = () => {
       setNotFound("No Data Found");
     }
   }, []);
-  console.log(donations);
-  console.log(notFound);
+
+  const handleRemoveAll = () => {
+    localStorage.clear();
+    setDonations([]);
+    setNotFound("No Data Found");
+  };
+  // console.log(donations);
+  // console.log(notFound);
 
   return (
     <div>
@@ -24,10 +31,31 @@ const Donation = () => {
           {notFound}
         </p>
       ) : (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-          {donations.map((card) => (
-            <DonatedItems key={card.id} card={card}></DonatedItems>
-          ))}
+        <div>
+          <button
+            onClick={handleRemoveAll}
+            className="block mx-auto bg-[#FF444A] px-4 py-1 rounded-sm 
+            text-white font-semibold mb-10"
+          >
+            Remove All
+          </button>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+            {isShowAll
+              ? donations.map((card) => (
+                  <DonatedItems key={card.id} card={card}></DonatedItems>
+                ))
+              : donations
+                  .slice(0, 4)
+                  .map((card) => (
+                    <DonatedItems key={card.id} card={card}></DonatedItems>
+                  ))}
+          </div>
+          <button
+            onClick={() => setIsShowAll(!isShowAll)}
+            className="bg-[#009444] px-4 py-1 text-white font-semibold rounded-sm block mx-auto mt-10"
+          >
+            {isShowAll ? "Show Less" : "Show More"}
+          </button>
         </div>
       )}
     </div>
